608cc10b17d494df4b6b90db9d97369005e9324d
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-04-19 21:00:40 +0300

    add lazy+suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
-import { Component } from 'react';
+import { Component, Suspense, lazy } from 'react';
 import AppBar from './components/AppBar';
-import HomeView from './pages/HomePage';
-import MovieView from './pages/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
 import routes from './routes';
 
 import './styles.css';
+
+const HomeView = lazy(() => import('./pages/HomePage' /* webpackChunkName: "home-page" */));
+const MovieView = lazy(() => import('./pages/MoviesPage' /* webpackChunkName: "movie-page" */));
+const MovieDetailsPage = lazy(() =>
+   import('./pages/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */),
+);
 
 class App extends Component {
    state = {};
@@ -15,11 +18,13 @@ class App extends Component {
       return (
          <div className="App">
             <AppBar />
-            <Switch>
-               <Route path={routes.movieDetauls} component={MovieDetailsPage} />
-               <Route path={routes.movies} component={MovieView} />
-               <Route path={routes.home} component={HomeView} />
-            </Switch>
+            <Suspense fallback={<h1>грузимся...</h1>}>
+               <Switch>
+                  <Route path={routes.movieDetauls} component={MovieDetailsPage} />
+                  <Route path={routes.movies} component={MovieView} />
+                  <Route path={routes.home} component={HomeView} />
+               </Switch>
+            </Suspense>
          </div>
       );
    }
